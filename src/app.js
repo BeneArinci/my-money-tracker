531ff3259090c22be5e3f0546b6e526a97a0d916
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-05-03 01:02:52 +0200

    Project setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import 'normalize.css'
+import './styles/styles.scss'
+
+
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(
+  <>App goes here</>
+)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  mode: 'development',
+}
