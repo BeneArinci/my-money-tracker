450c901074db320ceab73f969022971c72a32426
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-05-04 23:49:55 +0200

    Layout, router structure + first link

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'normalize.css'
+import { createRoot } from 'react-dom/client'
+import {
+  Link,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
 import './styles/styles.scss'
+
+const Header = (props) => (
+  <div>
+    <h1>This is my header</h1>
+    {props.children}
+  </div>
+)
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
 
 const ExpensesDashboardPage = () => {
   return <div>Hellooooooo</div>
@@ -20,18 +43,29 @@ const SupportPage = () => {
 }
 
 const ErrorPage = () => {
-  return <div>This is an error page</div>
+  return (
+    <div>
+      This is an error page - <Link to="/">Go back to the home page</Link>
+    </div>
+  )
 }
 
-const router = createBrowserRouter([
+const routes = [
   {
     path: '/',
     element: <ExpensesDashboardPage />,
-    errorElement: ErrorPage,
   },
   { path: '/add-expense', element: <AddExpensePage /> },
   { path: '/edit-expense', element: <EditExpensePage /> },
   { path: '/support', element: <SupportPage /> },
+  { path: '*', element: <ErrorPage /> },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: routes,
+  },
 ])
 
 createRoot(document.getElementById('app')).render(
