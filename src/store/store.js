70ec09b5c97cb4f902e736f1e71ba2e6a9b20fd2
Benedetta Arinci <benedetta.arinci@gmail.com>
2024-05-10 01:05:18 +0200

    expenses slice completed

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,67 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { v4 as uuidv4 } from 'uuid'
 
-console.log('test')
-
 const expensesSlice = createSlice({
   name: 'expenses',
   initialState: [],
   reducers: {
     addExpense: {
       reducer(state, action) {
-        state.push(action.payload)
+        return [...state, action.payload]
       },
-      prepare(title, description) {
+      prepare(title, description, amount) {
         return {
           payload: {
             id: uuidv4(),
             title,
             description,
-            amount: 0,
+            amount,
             createdAt: 0,
+          },
+        }
+      },
+    },
+    updateExpense: {
+      reducer(state, action) {
+        return state.map((expense) => {
+          console.log(action.payload)
+          if (expense.id === action.payload.id) {
+            return {
+              ...expense,
+              title: action.payload.title
+                ? action.payload.title
+                : expense.title,
+              description: action.payload.description
+                ? action.payload.description
+                : expense.description,
+              amount: action.payload.amount
+                ? action.payload.amount
+                : expense.amount,
+            }
+          }
+
+          return expense
+        })
+      },
+      prepare(id, title, description, amount) {
+        return {
+          payload: {
+            id,
+            title,
+            description,
+            amount,
+          },
+        }
+      },
+    },
+    deleteExpense: {
+      reducer(state, action) {
+        return state.filter((expense) => expense.id !== action.payload.id)
+      },
+      prepare(expenseId) {
+        return {
+          payload: {
+            id: expenseId,
           },
         }
       },
@@ -35,8 +78,25 @@ const store = configureStore({
 
 console.log(store.getState())
 
-const { addExpense } = expensesSlice.actions
+const { addExpense, deleteExpense, updateExpense } = expensesSlice.actions
 
-store.dispatch(addExpense('Test expense', 'description'))
+const firstExpense = store.dispatch(
+  addExpense('Test expense', 'description', 3000)
+)
+
+const secondExpense = store.dispatch(
+  addExpense('Second expense', 'some description', 45000)
+)
+
+store.dispatch(deleteExpense(firstExpense.payload.id))
+
+store.dispatch(
+  updateExpense(
+    secondExpense.payload.id,
+    'Updated title',
+    'updated description',
+    9000
+  )
+)
 
 console.log(store.getState())
